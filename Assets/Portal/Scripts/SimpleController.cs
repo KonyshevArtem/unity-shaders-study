34eff716e951c7e1f33a9aa27531dc1447bb4cd3
Artem Konyshev <konyshev_arte@mail.ru>
2020-04-18 22:22:35 +0300

    - add simple portal scene
    - add simple fps controller

---
+++ Assets/Portal/Scripts/SimpleController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SimpleController : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+    [SerializeField] private float speed;
+
+    private new Rigidbody rigidbody;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ControlLocomotion();
+        ControlCamera();
+    }
+
+    void ControlLocomotion()
+    {
+        Vector3 velocity =
+            (transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical")) * speed;
+        velocity.y = rigidbody.velocity.y;
+        rigidbody.velocity = velocity;
+    }
+
+    void ControlCamera()
+    {
+        transform.Rotate(Vector3.up, Input.GetAxis("Mouse X"));
+
+        float cameraAngle = Vector3.SignedAngle(transform.forward, camera.transform.forward, transform.right);
+        cameraAngle = Mathf.Clamp(cameraAngle - Input.GetAxis("Mouse Y"), -80, 80);
+        camera.transform.localRotation = Quaternion.AngleAxis(cameraAngle, Vector3.right);
+    }
+}
