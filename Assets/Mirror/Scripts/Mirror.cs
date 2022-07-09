487759e5365727cf4c5697a72954f6ecd6d402f4
Artem Konyshev <konyshev.ar@gmail.com>
2022-07-09 21:07:38 +0300

    implement recursive mirror rendering (1 level deep for now)

--- Assets/Mirror/Scripts/Mirror.cs
+++ Assets/Mirror/Scripts/Mirror.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
 
 [ExecuteInEditMode, RequireComponent(typeof(Renderer))]
 public class Mirror : MonoBehaviour
@@ -11,31 +9,16 @@ public class Mirror : MonoBehaviour
     RenderTexture m_Texture;
     MaterialPropertyBlock m_Properties;
 
+    public Camera Camera => m_Camera;
+    public RenderTexture Texture => m_Texture;
+
     void Awake()
     {
-        RenderPipelineManager.beginCameraRendering += BeginFrame;
-
-        m_Texture = new RenderTexture(
-                Mathf.Max(Screen.width, 1),
-                Mathf.Max(Screen.height, 1),
-                32,
-                SystemInfo.GetGraphicsFormat(UnityEngine.Experimental.Rendering.DefaultFormat.LDR)
-            );
-
         m_Renderer = GetComponent<Renderer>();
         m_Properties = new MaterialPropertyBlock();
-        m_Properties.SetTexture("_MirrorTex", m_Texture);
     }
 
-    void OnDestroy()
-    {
-        RenderPipelineManager.beginCameraRendering -= BeginFrame;
-
-        if (m_Texture != null)
-            m_Texture.Release();
-    }
-
-    void BeginFrame(ScriptableRenderContext _Context, Camera _Camera)
+    public void SetupCamera(Camera _Camera)
     {
         Transform mirrorTransform = transform;
         Vector3 mirrorPos = mirrorTransform.position;
@@ -52,8 +35,6 @@ public class Mirror : MonoBehaviour
         float projDistance = Vector3.Dot(dir, mirrorNormal);
         destTransform.position = sourceTransform.position - 2 * mirrorNormal * projDistance;
 
-        m_Renderer.SetPropertyBlock(m_Properties);
-        m_Camera.targetTexture = m_Texture;
         m_Camera.fieldOfView = _Camera.fieldOfView;
         m_Camera.aspect = _Camera.aspect;
 
@@ -61,7 +42,18 @@ public class Mirror : MonoBehaviour
         Vector3 mirrorPosCS = destViewMatrix.MultiplyPoint(mirrorPos);
         Vector3 mirrorNormalCS = destViewMatrix.MultiplyVector(mirrorNormal);
         m_Camera.projectionMatrix = m_Camera.CalculateObliqueMatrix(new Vector4(mirrorNormalCS.x, mirrorNormalCS.y, mirrorNormalCS.z, -Vector3.Dot(mirrorNormalCS, mirrorPosCS)));
+    }
 
-        UniversalRenderPipeline.RenderSingleCamera(_Context, m_Camera);
+    public void SetupTexture(RenderTexture _Texture)
+    {
+        m_Texture = _Texture;
+
+        if (_Texture != null)
+        {
+            m_Properties.SetTexture("_MirrorTex", _Texture);
+            m_Renderer.SetPropertyBlock(m_Properties);
+        }
+
+        m_Camera.targetTexture = _Texture;
     }
 }
---
+++ Assets/Mirror/Scripts/MirrorManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+[ExecuteInEditMode]
+class MirrorManager : MonoBehaviour
+{
+    const int MIRROR_MAX_DEPTH = 1;
+
+    Mirror[] m_Mirrors;
+    RenderTextureDescriptor m_Descriptor;
+
+    void Awake()
+    {
+        m_Mirrors = FindObjectsOfType<Mirror>(true);
+
+        m_Descriptor = new RenderTextureDescriptor(
+            Mathf.Max(Screen.width, 1),
+            Mathf.Max(Screen.height, 1),
+            SystemInfo.GetGraphicsFormat(UnityEngine.Experimental.Rendering.DefaultFormat.LDR),
+            32
+        );
+
+        RenderPipelineManager.beginCameraRendering += BeginRenderCamera;
+        RenderPipelineManager.endCameraRendering += EndRenderCamera;
+    }
+
+    void OnDestroy()
+    {
+        RenderPipelineManager.beginCameraRendering -= BeginRenderCamera;
+        RenderPipelineManager.endCameraRendering -= EndRenderCamera;
+    }
+
+    void BeginRenderCamera(ScriptableRenderContext _Context, Camera _Camera)
+    {
+        foreach (Mirror m in m_Mirrors)
+            RenderMirror(_Context, _Camera, m, 0);
+    }
+
+    void EndRenderCamera(ScriptableRenderContext _Context, Camera _Camera)
+    {
+        foreach (Mirror m in m_Mirrors)
+            ResetMirror(m, null);
+    }
+
+    void RenderMirror(ScriptableRenderContext _Context, Camera _Camera, Mirror _Mirror, int _Depth)
+    {
+        _Mirror.SetupCamera(_Camera);
+        _Mirror.SetupTexture(RenderTexture.GetTemporary(m_Descriptor));
+
+        Dictionary<Mirror, RenderTexture> oldTextures = new Dictionary<Mirror, RenderTexture>();
+
+        if (_Depth < MIRROR_MAX_DEPTH)
+        {
+            foreach (Mirror m in m_Mirrors.Where(_M => _M != _Mirror))
+            {
+                oldTextures[m] = m.Texture;
+
+                RenderMirror(_Context, _Mirror.Camera, m, _Depth + 1);
+            }
+        }
+
+        //_Mirror.Setup(_Camera, _Texture);
+        UniversalRenderPipeline.RenderSingleCamera(_Context, _Mirror.Camera);
+
+        if (_Depth < MIRROR_MAX_DEPTH)
+        {
+            foreach (Mirror m in m_Mirrors.Where(_M => _M != _Mirror))
+                ResetMirror(m, oldTextures[m]);
+        }
+    }
+
+    void ResetMirror(Mirror _Mirror, RenderTexture _OldTexture)
+    {
+        RenderTexture t = _Mirror.Texture;
+        _Mirror.SetupTexture(_OldTexture);
+        RenderTexture.ReleaseTemporary(t);
+    }
+}
